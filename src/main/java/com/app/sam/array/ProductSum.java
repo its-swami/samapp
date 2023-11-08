8e3d3453fa8d84502db593f1c279be2e65fa9454
Swaminathan Janarthanan <sjanarthanan@ebay.com>
2023-11-07 22:27:57 -0800

    7nov 2023

---
+++ src/main/java/com/app/sam/array/ProductSum.java
@@ -0,0 +1,24 @@
+package com.app.sam.array;
+
+public class ProductSum {
+    public static int productSum(Object[] array) {
+        return productSum(array, 1);
+    }
+
+    public static int productSum(Object[] array, int multiplier) {
+        int sum = 0;
+        for (Object element : array) {
+            if (element instanceof Object[]) {
+                sum += productSum((Object[]) element, multiplier + 1);
+            } else {
+                sum += (int) element;
+            }
+        }
+        return sum * multiplier;
+    }
+
+    public static void main(String args[]) {
+        Object[] array = new Object[]{5, 2, new Object[]{7, -1}, 3, new Object[]{6, new Object[]{-13, 8}, 4}};
+        System.out.println(productSum(array));
+    }
+}
---
+++ src/main/java/com/app/sam/str/RunLengthEncoding.java
@@ -0,0 +1,58 @@
+package com.app.sam.str;
+
+public class RunLengthEncoding {
+
+    /**
+     *
+     * @param str
+     * @return
+     */
+    public static String runLengthEncoding(String str) {
+        StringBuilder encodedStringCharacters = new StringBuilder();
+        int currentRunLength = 1;
+        char lastProcessed ;
+        for (int i = 1; i < str.length(); i++) {
+            char currentCharacter = str.charAt(i);
+            char previousCharacter = str.charAt(i - 1);
+            if (currentCharacter != previousCharacter || currentRunLength == 9) {
+                encodedStringCharacters.append(Integer.toString(currentRunLength));
+                encodedStringCharacters.append(previousCharacter);
+                currentRunLength = 0;
+            }
+            currentRunLength += 1;
+        }
+        encodedStringCharacters.append(Integer.toString(currentRunLength));
+        encodedStringCharacters.append(str.charAt(str.length() - 1));
+        return encodedStringCharacters.toString();
+    }
+
+    /**
+     *
+     * @param name
+     * @return
+     */
+    public static String encoding(String name) {
+        StringBuilder output = new StringBuilder();
+        if ( name == null || name.isEmpty() ){
+            return output.toString();
+        }
+        char lastProcessed = name.charAt(0);
+        int charCount = 1;
+        for (int i = 1; i < name.length(); i++) {
+            if (lastProcessed == name.charAt(i) && charCount < 9) {
+                charCount++;
+            } else {
+                output.append(charCount).append(lastProcessed);
+                lastProcessed = name.charAt(i);
+                charCount = 1;
+            }
+        }
+        output.append(charCount).append(lastProcessed);
+        return output.toString();
+    }
+
+    public static void main(String args[]) {
+        System.out.println(runLengthEncoding("AAAAAAAAAAAAABBCCCCDD"));
+        System.out.println(encoding("AAAAAAAAAAAAABBCCCCDD"));
+    }
+}
